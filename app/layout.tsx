d1e20565192a7624343ddd1752d79a53e2eea682
Eriquy <eriquy@gmail.com>
2025-01-02 16:24:11 -0300

    feat: change the application description

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Anti-Procrastination App',
+  description:
+    'A sleek and efficient application designed to demonstrate my expertise and passion for creating impactful digital solutions.',
 }
 
 const geistSans = Geist({
